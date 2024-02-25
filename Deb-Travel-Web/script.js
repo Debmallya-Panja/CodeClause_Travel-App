13bd29e04b39e279dc184a58ec5df84dc35262e9
Debmallya panja <140999209+Debmallya-Panja@users.noreply.github.com>
2024-02-25 16:56:44 +0530

    Add files via upload

---
+++ Deb-Travel-Web/script.js
@@ -0,0 +1,18 @@
+// <!-- Javascript -->
+// <script>
+//     function validateform(){ 
+//         var tel=document.getElementById("phonenum").value;  
+
+//         if(tel.length<10){  
+//             alert("Phone number must be of atleast 10 digits!");  
+//             return false;  
+//         } else if(isNaN(tel)){
+//             alert("Phone number should not include character!");
+//             return false;
+//         }
+//         return true;
+// }  
+// </script>
+
+// </body>
+// </html>
